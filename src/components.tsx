f1771fb88fcedaeff655d6ccc543909fbc71cb36
Tyler Long <tyler4long@gmail.com>
2023-04-04 16:45:14 -0700

    Add ant design but no style

--- src/components.tsx
+++ src/components.tsx
@@ -1,3 +1,11 @@
 import React from 'react';
+import { Button, Typography } from 'antd';
 
-export const App = () => <h1>Hello world!</h1>;
+const { Title } = Typography;
+
+export const App = () => (
+  <>
+    <Title>Hello world!</Title>
+    <Button type="primary">Click me!</Button>
+  </>
+);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { renderToString } from 'react-dom/server';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
 
 import { App } from './components';
 
-console.log(renderToString(App()));
+const html = renderToString(App());
+console.log(html);
+writeFileSync(join(__dirname, '..', 'docs', 'index.html'), html);
