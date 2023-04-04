c9f7ce2100ea107697acc7e99077aaa870b02efc
Tyler Long <tyler4long@gmail.com>
2023-04-04 16:57:03 -0700

    Try to add antd styles but failed

--- src/components.tsx
+++ src/components.tsx
@@ -4,8 +4,14 @@ import { Button, Typography } from 'antd';
 const { Title } = Typography;
 
 export const App = () => (
-  <>
-    <Title>Hello world!</Title>
-    <Button type="primary">Click me!</Button>
-  </>
+  <html>
+    <head>
+      <link rel="stylesheet" href="./reset.css" />
+    </head>
+    <body>
+      <Title>Hello world!</Title>
+      <Button type="primary">Click me!</Button>
+      <script src="./antd.min.js"></script>
+    </body>
+  </html>
 );
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { renderToString } from 'react-dom/server';
+import { renderToStaticMarkup } from 'react-dom/server';
 import { writeFileSync } from 'fs';
 import { join } from 'path';
 
 import { App } from './components';
 
-const html = renderToString(App());
+const html = renderToStaticMarkup(App());
 console.log(html);
 writeFileSync(join(__dirname, '..', 'docs', 'index.html'), html);
