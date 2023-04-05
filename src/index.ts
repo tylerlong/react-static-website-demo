645bef1ee62bb23859ae04e1feada857bd7ba595
Tyler Long <tyler4long@gmail.com>
2023-04-04 17:00:46 -0700

    A new parcel website

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { renderToStaticMarkup } from 'react-dom/server';
-import { writeFileSync } from 'fs';
-import { join } from 'path';
-
-import { App } from './components';
-
-const html = renderToStaticMarkup(App());
-console.log(html);
-writeFileSync(join(__dirname, '..', 'docs', 'index.html'), html);
